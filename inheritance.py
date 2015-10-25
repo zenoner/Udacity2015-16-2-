69d0e57a779e54d8073beda0a4790ccffcd1967c
Seiji Mendoza <srm5149@gmail.com>
2015-10-25 12:50:46 -0400

    Update inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -4,11 +4,20 @@ class Parent():
         self.last_name = last_name
         self.eye_color = eye_color
         
+    def show_info(self):
+        print("Last Name - "+self.last_name) # Last Name - Cyrus 
+        print("Eye Color - "+self.eye_color) #Eye Color - Blue
+        
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
         print("Child Constructor Called)
         Parent.__init__(self, last_name, eye_color)
         self.number_of_toys = number_of_toys
+        
+        def show_info(self):
+            print("Last Name - "+self.last_name) 
+            print("Eye Color - "+self.eye_color)
+            print("Number of toys - "str(self.number_of_toys)) #Method Overriding  Number of toys - 5
 
 
 billy_cyrus = Parent("Cyrus", "blue")
