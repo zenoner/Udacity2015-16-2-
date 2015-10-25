cdf1b56c339666a7c33b6e81910b83466e1cf1c5
Seiji Mendoza <srm5149@gmail.com>
2015-10-25 11:30:33 -0400

    Create inheritance.py

---
+++ inheritance.py
@@ -0,0 +1,19 @@
+class Parent():
+    def __init__(self, last_name, eye_color):
+        print("Parent Constructor Called")
+        self.last_name = last_name
+        self.eye_color = eye_color
+        
+class Child(Parent):
+    def __init__(self, last_name, eye_color, number_of_toys):
+        print("Child Constructor Called)
+        Parent.__init__(self, last_name, eye_color)
+        self.number_of_toys = number_of_toys
+
+
+billy_cyrus = Parent("Cyrus", "blue")
+print(billy_cyrus.last_name)   #output Cyrus
+
+miley_cyrus = Child("Cyrus", "blue", 5)
+print(miley_cyrus.last_name) #Cyrus
+print(miley_cyrus.number_of_toys) # 5
